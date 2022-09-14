f6ff986c0b18eb9d0dd27312f2de3c38ece7f4b1
aortegaf <se.aortegaf@github.com>
2022-09-14 14:37:34 -0500

    player pet selection & player attacks creation

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,15 @@
+let playerAttack
+
 function startGame(){
     let petButton = document.getElementById("pet-button")
     petButton.addEventListener("click", playerPetSelection)
+
+    let fireButton = document.getElementById("fire-button")
+    fireButton.addEventListener("click", fireAttack)
+    let waterButton = document.getElementById("water-button")
+    waterButton.addEventListener("click", waterAttack)
+    let groundButton = document.getElementById("ground-button")
+    groundButton.addEventListener("click", groundAttack)
 }
 
 function playerPetSelection(){
@@ -8,7 +17,7 @@ function playerPetSelection(){
     let inputSplash = document.getElementById("splash")
     let inputGrounda = document.getElementById("grounda")
     let spanPlayerPet = document.getElementById("player-pet")
-
+    
     if(inputFyre.checked){
         spanPlayerPet.innerHTML = "Fyre"
     } else if (inputSplash.checked){
@@ -18,6 +27,40 @@ function playerPetSelection(){
     } else {
         alert("No has seleccionado mascota")
     }
+
+    enemyPetSelection()
+}
+
+function random(min, max){
+    return Math.floor(Math.random() * (max-min+1) + min)
+}
+
+function enemyPetSelection(){
+    let petCode = random(1,3)
+    let spanEnemyPet = document.getElementById("enemy-pet")
+
+    if(petCode == 1){
+        spanEnemyPet.innerHTML = "Fyre"
+    } else if (petCode == 2){
+        spanEnemyPet.innerHTML = "Splash"
+    } else if (petCode == 3){
+        spanEnemyPet.innerHTML = "Grounda"
+    }
+}
+
+function fireAttack(){
+    playerAttack = "Fuego 🔥"
+    alert(playerAttack)
+}
+
+function waterAttack(){
+    playerAttack = "Agua 💧"
+    alert(playerAttack)
+}
+
+function groundAttack(){
+    playerAttack = "Tierra 🌱"
+    alert(playerAttack)
 }
 
 window.addEventListener("load", startGame)
